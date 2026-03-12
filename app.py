7777b0827ef9c0c2b18aec726445a64f0568f071
Divnia-2005 <diviniamarioantony2028@mca.ajce.in>
2026-03-12 15:35:27 +0530

    fixed port

--- app.py
+++ app.py
@@ -2425,5 +2425,6 @@ def update_system_config():
         conn.close()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
